db80d4fcc5018230e83648446b4a2f4547b94891
Reyhan Nandita Al Zahra <89372014+alzahrareyhan@users.noreply.github.com>
2025-09-01 15:08:18 +0700

    project new

--- data/projects.ts
+++ data/projects.ts
@@ -8,6 +8,38 @@ export interface Project {
 
 export const projects: Project[] = [
   {
+    title: 'Mango Leaf Disease Detection',
+    description:
+      'Mango leaf disease detection using feature fusion of ResNet50V2 and EfficientNetV2, achieving 99.75% accuracy.',
+    logo: '/logos/Py.png',
+    link: 'https://www.kaggle.com/code/reyhannandita/mango-feature-fusion-resnet50v2-and-efficientnetv2',
+    slug: 'Mango Leaf Disease Detection',
+  },
+  {
+    title: 'Computer Vision - Trash Detection',
+    description:
+      'An intelligent and efficient way to detect and classify trash using computer vision for a cleaner environment.',
+    logo: '/logos/Py.png',
+    link: 'https://github.com/alzahrareyhan/Computer-Vision-Trash-Detection',
+    slug: 'Computer Vision - Trash Detection',
+  },
+  {
+    title: 'Sentiment Analysis Film Review',
+    description:
+      'An NLP model that processes and classifies film reviews into positive, negative, or neutral sentiments with 89% accuracy.',
+    logo: '/logos/Py.png',
+    link: 'https://www.kaggle.com/code/reyhannandita/sentiment-analysis-film-imdb-dataset',
+    slug: 'Sentiment Analysis Film Review',
+  },
+    {
+    title: 'House Price Prediction',
+    description:
+      'A machine learning model that predicts house prices based on features such as location, size, and room count with 90% accuracy.',
+    logo: '/logos/Py.png',
+    link: 'https://www.kaggle.com/code/reyhannandita/house-price-prediction',
+    slug: 'House Price Prediction',
+  },
+  {
     title: 'SKM Kabupaten Cirebon',
     description:
       'Public Satisfaction Survey Portal for Cirebon Regency. A Laravel-based system for efficient, transparent, and integrated SKM submission, management, and analysis.',
@@ -34,36 +66,20 @@ export const projects: Project[] = [
     slug: 'Portofolio - UI/UX Designer & Web Developer',
   },
 
-  {
-    title: 'Computer Vision - Trash Detection',
-    description:
-      'An intelligent and efficient way to detect and classify trash using computer vision for a cleaner environment.',
-    logo: '/logos/Py.png',
-    link: 'https://github.com/alzahrareyhan/Computer-Vision-Trash-Detection',
-    slug: 'Computer Vision - Trash Detection',
-  },
 
   {
     title: 'Voyage',
     description:
-      'Navigate Bali by bus with live schedules, real-time tracking, and easy booking—built with React for a fast and seamless travel experience',
+      'Navigate Bali by bus with live schedules, real-time tracking, and easy booking—built with React for a fast and seamless travel experience.',
     logo: '/logos/react_icon.svg',
     link: 'https://github.com/alzahrareyhan/Voyage',
     slug: 'Voyage',
   },
 
   {
-    title: 'Mango Leaf Disease Detection',
-    description:
-      'Mango leaf disease detection using feature fusion of ResNet50V2 and EfficientNetV2, achieving 99.75% accuracy.',
-    logo: '/logos/Py.png',
-    link: 'https://www.kaggle.com/code/reyhannandita/mango-feature-fusion-resnet50v2-and-efficientnetv2',
-    slug: 'Mango Leaf Disease Detection',
-  },
-  {
     title: 'Your Skin - Beatuy Care Prototype',
     description:
-      'A prototype beauty care platform offering doctor consultations, skincare tips, skin disease info, and mental health support in one place',
+      'All-in-one platform for skincare, doctor consults, and mental health support.',
     logo: '/logos/figma.svg',
     link: 'https://www.figma.com/proto/MKrAx1HtZpTQU3GsA2OipT/Beauty-Care-Prototype?node-id=416-6896&p=f&t=hnPAc023bLUny8fp-0&scaling=scale-down&content-scaling=fixed&page-id=145%3A3339&starting-point-node-id=416%3A6896',
     slug: 'Your Skin - Beatuy Care Prototype',
